798ee7050231c8a591385445e6836d0d24dfd76e
Franco_mrs <franco.m.silva@aluno.senai.br>
2023-04-19 17:53:01 -0300

    first commit

---
+++ testeverdurao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeverdurao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Meudesenho();
+            
+           
+            Console.ReadLine();
+           
+            
+        }
+
+        public static void Meudesenho()
+        {
+          
+            int i = 0;
+            string espaco = "";
+
+
+
+            Console.WriteLine("_______________________________");
+
+            while (i < 4)
+
+            {
+                Console.WriteLine(espaco);
+                i++;
+            }
+
+            Console.WriteLine("_______________________________");
+
+        }
+    }
+
+        
+
+ }
+
+
